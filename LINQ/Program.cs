61873e32c9b407d20113cf0af9e36bc0cabf9de2
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-31 14:25:14 -0500

    Exercise completed

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var gameNames = new List<string>() 
+            {
+                "Fallout: New Vegas",
+                "Call of Duty",
+                "Star Wars: Battlefront 2",
+                "Fall Guys",
+                "Fallout 3",
+                "Legend Of Zelda: Breath of the Wild",
+                "Paladins"
+            };
+
+            var orderedGames = gameNames.OrderBy(game => game.Length).ToList();
+            orderedGames.ForEach(game => Console.WriteLine(game));
         }
     }
 }
